b4636bc8fafa3ced28c8c11b7ba33226546c30d9
gabe <gmbecker@ucdavis.edu>
2011-08-17 07:18:19 -0700

    R script tags work, including calling back up to javascript. Event handlers/calling R from webpage javascript doesn't work yet. \nExamples/tests somewhat cleaned up to work with new paradigm. Sliders probably still don't work.

--- inst/javascript/Rhelpers.js
+++ inst/javascript/Rhelpers.js
@@ -1,18 +1,30 @@
+
 function jsREventHandler( name, value)
 {
-    netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
-    var out;
-    Rev.call1(name, value,  out);
-}
 
+    
+    //build event to pass to chrome.
+    var evt = document.createEvent("Events");
+    evt.initEvent("RFirefoxCall", true, false);
+    //Add our data
+    var RFirefoxData = document.getElementById("RFirefoxCallData");
+    RFirefoxData.setAttribute("FunName", name);
+    RFirefoxData.setAttribute("FunValue", value);
+    evt.RFunName = name;
+    evt.RFunValue = value;
+    RFirefoxData.dispatchEvent(evt);
+    
+    //    Rev.call1(name, value,  out);
+}
+/*
 function addREventHandler( name, targ, event, arg)
 {
     var fun = function() {
-	netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
 	var out;
 	Rev.call1(name, arg,  out);
     }
     targ.addEventListener(event, fun, true);    
 }
-	
+*/
+
 
