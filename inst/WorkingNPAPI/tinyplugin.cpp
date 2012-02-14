64393454b5df0c9e8159aec0f9f8716b2fcb4566
gabe <gmbecker@ucdavis.edu>
2012-02-14 15:32:40 -0800

    tinyplugin can start R and is calling NPP_New and NPP_GetValue, crashing when firefox is closed

--- inst/WorkingNPAPI/tinyplugin.cpp
+++ inst/WorkingNPAPI/tinyplugin.cpp
@@ -2,6 +2,11 @@
 #include <npruntime.h>
 #include <npfunctions.h>
 #include <stdio.h>
+#include <R.h>
+#include <Rdefines.h>
+#include <Rembedded.h>
+#include <plugin.h>
+
 // Register mime types and description for UNIX
 // (Windows declares it in resources)
 // Plugin's mime types
@@ -14,15 +19,21 @@ NPError NP_GetEntryPoints(NPPluginFuncs* pFuncs);
 NPError OSCALL NP_GetValue(void*, NPPVariable, void* out) {
   return NPERR_NO_ERROR;
 }
+static int isInitialized=0;
+int initR( const char **args, int nargs);
 // Initializes plugin
 NPError NP_Initialize(NPNetscapeFuncs* npnfuncs, NPPluginFuncs* nppfuncs) {
-  if(npnfuncs == NULL)
+if (nppfuncs->size < (offsetof(NPPluginFuncs, setvalue) + sizeof(void*)))
     return NPERR_INVALID_FUNCTABLE_ERROR;
-  fprintf(stderr, "Plugin Initialized:");fflush(stderr);
-  // TODO save npnfuncs pointer
+
+  const char *arg = "R";
+    
+  initR( &arg , 1);
+
   NP_GetEntryPoints(nppfuncs);
   return NPERR_NO_ERROR;
 }
+
 // Set table of functions called by browser.
 NPError NP_GetEntryPoints(NPPluginFuncs* pFuncs) {
   if (pFuncs == NULL)
@@ -30,6 +41,7 @@ NPError NP_GetEntryPoints(NPPluginFuncs* pFuncs) {
   pFuncs->version       = (NP_VERSION_MAJOR << 8) | NP_VERSION_MINOR;
   pFuncs->newp          = NPP_New;
   pFuncs->javaClass     = NULL;
+  pFuncs->getvalue = NPP_GetValue;
   return NPERR_NO_ERROR;
 }
 // new plugin instance
@@ -39,4 +51,78 @@ NPError NPP_New(NPMIMEType    pluginType,
                 char *argv[], NPSavedData *saved) {
   fprintf(stderr, "NPP_New");fflush(stderr);
   return NPERR_NO_ERROR;
+}
+
+
+NPError
+NPP_GetValue(NPP instance, NPPVariable variable, void *value) {
+  fprintf(stderr, "In NPP_GetValue");fflush(stderr);
+	if(instance == NULL)
+    return NPERR_INVALID_INSTANCE_ERROR;
+
+  NPError rv = NPERR_NO_ERROR;
+
+  if(instance == NULL)
+    return NPERR_GENERIC_ERROR;
+  
+  CPlugin * plugin = (CPlugin *)instance->pdata;
+  if(plugin == NULL)
+    return NPERR_GENERIC_ERROR;
+
+  switch (variable) 
+	{
+		case NPPVpluginWindowBool:
+			*((bool *)value) = true;
+			break;
+	
+		case NPPVpluginNameString:
+			*((char **)value) = "Boilerplate Plugin";
+			break;
+  
+		case NPPVpluginDescriptionString:
+			*((char **)value) = "Boilerplate web plugin";
+			break;
+
+		case NPPVpluginScriptableNPObject:
+      fprintf(stderr, "case NPPVpluginScriptableNPObject");fflush(stderr);
+			if (!plugin->isInitialized())
+			{
+				return NPERR_GENERIC_ERROR;
+			}
+			
+			*((NPObject **)value) = plugin->GetScriptableObject();
+
+			break;
+  
+		default:
+			rv = NPERR_GENERIC_ERROR;
+			break;
+  }
+  
+  return rv;
+  
+}
+
+NPError
+NPP_SetValue(NPP instance, NPNVariable variable, void *value) {
+  fprintf(stderr, "In NPP_SetValue");fflush(stderr);
+  return NPERR_GENERIC_ERROR;
+}
+
+int initR( const char **args, int nargs)
+{
+  char **rargs;
+  unsigned int i;
+  
+  if(isInitialized)
+    return 0;
+  
+  rargs = (char **) malloc(nargs * sizeof(char *));
+  for(i = 0 ; i < nargs; i++)
+    rargs[i] = strdup(args[i]);
+  fprintf(stderr, "Attempting to start embedded R.\n");fflush(stderr);
+  Rf_initEmbeddedR(nargs, rargs);
+  fprintf(stderr, "R initialization done.\n"); fflush(stderr);
+  
+  return 0;
 }
