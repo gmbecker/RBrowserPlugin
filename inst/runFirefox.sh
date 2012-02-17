12abf68dbeb036457c1a5d7621fd424c67cc5f8a
Duncan <duncan@unknown58b0357a07bf.gateway.2wire.net>
2012-02-17 09:35:01 -0800

    fix invokeRCall.html

--- inst/runFirefox.sh
+++ inst/runFirefox.sh
@@ -1,16 +1,24 @@
-FF=/home/gmbecker/local/firefox5.0-debug/bin/firefox
-FFOPTS="-g --no-remote -P dev"
+FF=/Applications/Firefox6.app/Contents/MacOS/firefox-bin
+IS_MAC=1
+FFOPTS="--no-remote -P dev"
+ARCH=x86_64
 
 if test -z "$R_HOME"
 then
-export R_HOME=/usr/lib64/R
+export R_HOME=/Library/Frameworks/R.framework/Resources
 fi
 
-if test -z "$LD_ADDED"
+if [ "$IS_MAC" = "1"]
 then
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$R_HOME/lib
-export LD_ADDED=1
+FFDEBUG=""
+R_LIB_LOC=$R_HOME/lib/$ARCH
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$R_LIB_LOC
+else
+FFDEBUG="-g"
+R_LIB_LOC=$R_HOME/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$R_LIB_LOC
 fi
 
-$FF $FFOPTS
 
+
+$FF $FFDEBUG $FFOPTS
