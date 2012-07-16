b1e6ef252e2574ed167292bad9d45aad79b65d6a
gabe <gmbecker@ucdavis.edu>
2012-07-16 09:58:44 -0700

    utilities.R

---
+++ R/utilities.R
@@ -0,0 +1,33 @@
+isRefClass = function(obj)
+  {
+    is(obj, "envRefClass")
+  }
+
+getFields = function(obj)
+  {
+    obj$getRefClass()$fields()
+  }
+
+getMethods = function(obj)
+  {
+    obj$getRefClass()$methods()
+  }
+
+
+checkForField = function(obj, name)
+  {
+    fields = getFields(obj)
+    return(name %in% names(fields))
+  }
+
+checkForMethod = function(obj, name)
+  {
+    methods = getMethods(obj)
+    return(name %in% methods)
+  }
+
+checkForFieldorMethod = function(obj, name)
+  {
+
+    return(checkForField(obj, name) || checkForMethod(obj, name))
+  }
