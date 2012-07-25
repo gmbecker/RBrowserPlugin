ed5d41e3982b98f6bacc3e03a36e602f975d1889
gabe <gmbecker@ucdavis.edu>
2012-07-25 11:09:40 -0700

    adding WebRObjectClasses.h

---
+++ inst/NPAPI/WebRObjectClasses.h
@@ -0,0 +1,365 @@
+#ifndef WebRObjects_h_
+#define WebRObjects_h_
+
+
+#include "npapi.h"
+#include "npfunctions.h"
+#include <npruntime.h>
+#include <stdio.h>
+
+
+#ifndef XP_MACOSX
+#include <R.h>
+#include <Rdefines.h>
+#include <Rembedded.h>
+#include <Rinterface.h>
+#else
+#include <R/R.h>
+#include <R/Rdefines.h>
+#include <R/Rembedded.h>
+#include <R/Rinterface.h>
+#endif
+
+
+class RObject : public NPObject
+{
+protected:
+    // Class member functions that do the real work
+    void Deallocate();
+    void Invalidate();
+    bool HasMethod(NPIdentifier name);
+    bool Invoke(NPIdentifier name, const NPVariant *args, uint32_t argCount, NPVariant *result);
+    bool InvokeDefault(const NPVariant *args, uint32_t argCount, NPVariant *result);
+    bool HasProperty(NPIdentifier name);
+    bool GetProperty(NPIdentifier name, NPVariant *result);
+    bool SetProperty(NPIdentifier name, const NPVariant *value);
+    bool RemoveProperty(NPIdentifier name);
+    bool Enumerate(NPIdentifier **identifier, uint32_t *count);
+    bool Construct(const NPVariant *args, uint32_t argCount, NPVariant *result);
+
+public:
+  RObject (NPP instance);
+    		void Detatch (void);
+
+		// This is the method used to create the NPObject
+    // This method should not be called explicitly
+    // Instead, use NPN_CreateObject
+    static NPObject* Allocate(NPP npp, NPClass *aClass);
+
+    /////////////////////////////
+    // Static NPObject methods //
+    /////////////////////////////
+    static void _Deallocate(NPObject *npobj);
+    static void _Invalidate(NPObject *npobj);
+    static bool _HasMethod(NPObject *npobj, NPIdentifier name);
+    static bool _Invoke(NPObject *npobj, NPIdentifier name, const NPVariant *args, uint32_t argCount, NPVariant *result);
+    static bool _InvokeDefault(NPObject *npobj, const NPVariant *args, uint32_t argCount, NPVariant *result);
+    static bool _HasProperty(NPObject * npobj, NPIdentifier name);
+    static bool _GetProperty(NPObject *npobj, NPIdentifier name, NPVariant *result);
+    static bool _SetProperty(NPObject *npobj, NPIdentifier name, const NPVariant *value);
+    static bool _RemoveProperty(NPObject *npobj, NPIdentifier name);
+    static bool _Enumerate(NPObject *npobj, NPIdentifier **identifier, uint32_t *count);
+    static bool _Construct(NPObject *npobj, const NPVariant *args, uint32_t argCount, NPVariant *result);
+ 
+  static NPClass _npclass;
+    
+  SEXP object;
+  NPObject *converter;
+  NPP instance;
+  //Do we really need this? we seem to in order to be able to create references from the R side...
+  NPNetscapeFuncs *funcs;
+protected:
+  NPP m_Instance;
+	
+    
+};
+
+
+class RFunction : public NPObject
+{
+protected:
+    // Class member functions that do the real work
+    void Deallocate();
+    void Invalidate();
+    bool HasMethod(NPIdentifier name);
+    bool Invoke(NPIdentifier name, const NPVariant *args, uint32_t argCount, NPVariant *result);
+    bool InvokeDefault(const NPVariant *args, uint32_t argCount, NPVariant *result);
+    bool HasProperty(NPIdentifier name);
+    bool GetProperty(NPIdentifier name, NPVariant *result);
+    bool SetProperty(NPIdentifier name, const NPVariant *value);
+    bool RemoveProperty(NPIdentifier name);
+    bool Enumerate(NPIdentifier **identifier, uint32_t *count);
+    bool Construct(const NPVariant *args, uint32_t argCount, NPVariant *result);
+
+public:
+  RFunction (NPP instance);
+    		void Detatch (void);
+
+		// This is the method used to create the NPObject
+    // This method should not be called explicitly
+    // Instead, use NPN_CreateObject
+    static NPObject* Allocate(NPP npp, NPClass *aClass);
+
+    /////////////////////////////
+    // Static NPObject methods //
+    /////////////////////////////
+    static void _Deallocate(NPObject *npobj);
+    static void _Invalidate(NPObject *npobj);
+    static bool _HasMethod(NPObject *npobj, NPIdentifier name);
+    static bool _Invoke(NPObject *npobj, NPIdentifier name, const NPVariant *args, uint32_t argCount, NPVariant *result);
+    static bool _InvokeDefault(NPObject *npobj, const NPVariant *args, uint32_t argCount, NPVariant *result);
+    static bool _HasProperty(NPObject * npobj, NPIdentifier name);
+    static bool _GetProperty(NPObject *npobj, NPIdentifier name, NPVariant *result);
+    static bool _SetProperty(NPObject *npobj, NPIdentifier name, const NPVariant *value);
+    static bool _RemoveProperty(NPObject *npobj, NPIdentifier name);
+    static bool _Enumerate(NPObject *npobj, NPIdentifier **identifier, uint32_t *count);
+    static bool _Construct(NPObject *npobj, const NPVariant *args, uint32_t argCount, NPVariant *result);
+ 
+  static NPClass _npclass;
+    
+  SEXP object;
+  NPObject *converter;
+  NPP instance;
+  //Do we really need this? we seem to in order to be able to create references from the R side...
+  NPNetscapeFuncs *funcs;
+protected:
+  NPP m_Instance;
+	
+
+};
+
+
+class RS4Object : public NPObject
+{
+protected:
+    // Class member functions that do the real work
+    void Deallocate();
+    void Invalidate();
+    bool HasMethod(NPIdentifier name);
+    bool Invoke(NPIdentifier name, const NPVariant *args, uint32_t argCount, NPVariant *result);
+    bool InvokeDefault(const NPVariant *args, uint32_t argCount, NPVariant *result);
+    bool HasProperty(NPIdentifier name);
+    bool GetProperty(NPIdentifier name, NPVariant *result);
+    bool SetProperty(NPIdentifier name, const NPVariant *value);
+    bool RemoveProperty(NPIdentifier name);
+    bool Enumerate(NPIdentifier **identifier, uint32_t *count);
+    bool Construct(const NPVariant *args, uint32_t argCount, NPVariant *result);
+
+public:
+  RS4Object(NPP instance);
+    		void Detatch (void);
+
+		// This is the method used to create the NPObject
+    // This method should not be called explicitly
+    // Instead, use NPN_CreateObject
+    static NPObject* Allocate(NPP npp, NPClass *aClass);
+
+    /////////////////////////////
+    // Static NPObject methods //
+    /////////////////////////////
+    static void _Deallocate(NPObject *npobj);
+    static void _Invalidate(NPObject *npobj);
+    static bool _HasMethod(NPObject *npobj, NPIdentifier name);
+    static bool _Invoke(NPObject *npobj, NPIdentifier name, const NPVariant *args, uint32_t argCount, NPVariant *result);
+    static bool _InvokeDefault(NPObject *npobj, const NPVariant *args, uint32_t argCount, NPVariant *result);
+    static bool _HasProperty(NPObject * npobj, NPIdentifier name);
+    static bool _GetProperty(NPObject *npobj, NPIdentifier name, NPVariant *result);
+    static bool _SetProperty(NPObject *npobj, NPIdentifier name, const NPVariant *value);
+    static bool _RemoveProperty(NPObject *npobj, NPIdentifier name);
+    static bool _Enumerate(NPObject *npobj, NPIdentifier **identifier, uint32_t *count);
+    static bool _Construct(NPObject *npobj, const NPVariant *args, uint32_t argCount, NPVariant *result);
+ 
+  static NPClass _npclass;
+    
+  SEXP object;
+  NPObject *converter;
+  NPP instance;
+  //Do we really need this? we seem to in order to be able to create references from the R side...
+  NPNetscapeFuncs *funcs;
+protected:
+  NPP m_Instance;
+};
+
+
+class RRefClassObject : public NPObject
+{
+protected:
+    // Class member functions that do the real work
+    void Deallocate();
+    void Invalidate();
+    bool HasMethod(NPIdentifier name);
+    bool Invoke(NPIdentifier name, const NPVariant *args, uint32_t argCount, NPVariant *result);
+    bool InvokeDefault(const NPVariant *args, uint32_t argCount, NPVariant *result);
+    bool HasProperty(NPIdentifier name);
+    bool GetProperty(NPIdentifier name, NPVariant *result);
+    bool SetProperty(NPIdentifier name, const NPVariant *value);
+    bool RemoveProperty(NPIdentifier name);
+    bool Enumerate(NPIdentifier **identifier, uint32_t *count);
+    bool Construct(const NPVariant *args, uint32_t argCount, NPVariant *result);
+
+public:
+  RRefClassObject(NPP instance);
+    		void Detatch (void);
+
+		// This is the method used to create the NPObject
+    // This method should not be called explicitly
+    // Instead, use NPN_CreateObject
+    static NPObject* Allocate(NPP npp, NPClass *aClass);
+
+    /////////////////////////////
+    // Static NPObject methods //
+    /////////////////////////////
+    static void _Deallocate(NPObject *npobj);
+    static void _Invalidate(NPObject *npobj);
+    static bool _HasMethod(NPObject *npobj, NPIdentifier name);
+    static bool _Invoke(NPObject *npobj, NPIdentifier name, const NPVariant *args, uint32_t argCount, NPVariant *result);
+    static bool _InvokeDefault(NPObject *npobj, const NPVariant *args, uint32_t argCount, NPVariant *result);
+    static bool _HasProperty(NPObject * npobj, NPIdentifier name);
+    static bool _GetProperty(NPObject *npobj, NPIdentifier name, NPVariant *result);
+    static bool _SetProperty(NPObject *npobj, NPIdentifier name, const NPVariant *value);
+    static bool _RemoveProperty(NPObject *npobj, NPIdentifier name);
+    static bool _Enumerate(NPObject *npobj, NPIdentifier **identifier, uint32_t *count);
+    static bool _Construct(NPObject *npobj, const NPVariant *args, uint32_t argCount, NPVariant *result);
+ 
+  static NPClass _npclass;
+    
+  SEXP object;
+  NPObject *converter;
+  NPP instance;
+  //Do we really need this? we seem to in order to be able to create references from the R side...
+  NPNetscapeFuncs *funcs;
+protected:
+  NPP m_Instance;
+};
+
+
+/*
+class RVector : public NPObject
+{
+protected:
+    // Class member functions that do the real work
+    void Deallocate();
+    void Invalidate();
+    bool HasMethod(NPIdentifier name);
+    bool Invoke(NPIdentifier name, const NPVariant *args, uint32_t argCount, NPVariant *result);
+    bool InvokeDefault(const NPVariant *args, uint32_t argCount, NPVariant *result);
+    bool HasProperty(NPIdentifier name);
+    bool GetProperty(NPIdentifier name, NPVariant *result);
+    bool SetProperty(NPIdentifier name, const NPVariant *value);
+    bool RemoveProperty(NPIdentifier name);
+    bool Enumerate(NPIdentifier **identifier, uint32_t *count);
+    bool Construct(const NPVariant *args, uint32_t argCount, NPVariant *result);
+
+public:
+  RVector(NPP instance);
+    		void Detatch (void);
+
+		// This is the method used to create the NPObject
+    // This method should not be called explicitly
+    // Instead, use NPN_CreateObject
+    static NPObject* Allocate(NPP npp, NPClass *aClass);
+
+    /////////////////////////////
+    // Static NPObject methods //
+    /////////////////////////////
+    static void _Deallocate(NPObject *npobj);
+    static void _Invalidate(NPObject *npobj);
+    static bool _HasMethod(NPObject *npobj, NPIdentifier name);
+    static bool _Invoke(NPObject *npobj, NPIdentifier name, const NPVariant *args, uint32_t argCount, NPVariant *result);
+    static bool _InvokeDefault(NPObject *npobj, const NPVariant *args, uint32_t argCount, NPVariant *result);
+    static bool _HasProperty(NPObject * npobj, NPIdentifier name);
+    static bool _GetProperty(NPObject *npobj, NPIdentifier name, NPVariant *result);
+    static bool _SetProperty(NPObject *npobj, NPIdentifier name, const NPVariant *value);
+    static bool _RemoveProperty(NPObject *npobj, NPIdentifier name);
+    static bool _Enumerate(NPObject *npobj, NPIdentifier **identifier, uint32_t *count);
+    static bool _Construct(NPObject *npobj, const NPVariant *args, uint32_t argCount, NPVariant *result);
+ 
+  static NPClass _npclass;
+    
+  SEXP object;
+  NPObject *converter;
+  NPP instance;
+  //Do we really need this? we seem to in order to be able to create references from the R side...
+  NPNetscapeFuncs *funcs;
+protected:
+  NPP m_Instance;
+  int vecType;
+};
+*/
+
+class RSubsettableObject : public NPObject
+{
+
+protected:
+    // Class member functions that do the real work
+    void Deallocate();
+    void Invalidate();
+    bool HasMethod(NPIdentifier name);
+    virtual bool Invoke(NPIdentifier name, const NPVariant *args, uint32_t argCount, NPVariant *result);
+    bool InvokeDefault(const NPVariant *args, uint32_t argCount, NPVariant *result);
+    bool HasProperty(NPIdentifier name);
+    bool GetProperty(NPIdentifier name, NPVariant *result);
+    bool SetProperty(NPIdentifier name, const NPVariant *value);
+    bool RemoveProperty(NPIdentifier name);
+    bool Enumerate(NPIdentifier **identifier, uint32_t *count);
+    bool Construct(const NPVariant *args, uint32_t argCount, NPVariant *result);
+
+public:
+    void Detatch (void);
+
+		// This is the method used to create the NPObject
+    // This method should not be called explicitly
+    // Instead, use NPN_CreateObject
+    //This is object type (class) specific
+    //static NPObject* Allocate(NPP npp, NPClass *aClass);
+
+    /////////////////////////////
+    // Static NPObject methods //
+    /////////////////////////////
+    static void _Deallocate(NPObject *npobj);
+    static void _Invalidate(NPObject *npobj);
+    static bool _HasMethod(NPObject *npobj, NPIdentifier name);
+    static bool _Invoke(NPObject *npobj, NPIdentifier name, const NPVariant *args, uint32_t argCount, NPVariant *result);
+    static bool _InvokeDefault(NPObject *npobj, const NPVariant *args, uint32_t argCount, NPVariant *result);
+    static bool _HasProperty(NPObject * npobj, NPIdentifier name);
+    static bool _GetProperty(NPObject *npobj, NPIdentifier name, NPVariant *result);
+    static bool _SetProperty(NPObject *npobj, NPIdentifier name, const NPVariant *value);
+    static bool _RemoveProperty(NPObject *npobj, NPIdentifier name);
+    static bool _Enumerate(NPObject *npobj, NPIdentifier **identifier, uint32_t *count);
+    static bool _Construct(NPObject *npobj, const NPVariant *args, uint32_t argCount, NPVariant *result);
+ 
+ public:
+    SEXP object;
+    NPObject *converter;
+    NPP instance;
+  //Do we really need this? we seem to in order to be able to create references from the R side...
+    NPNetscapeFuncs *funcs;
+    
+ protected:
+    NPP m_Instance;
+ };
+
+class RList : public RSubsettableObject
+{
+
+ public:
+  RList(NPP instance);
+  bool Invoke(NPIdentifier name, const NPVariant *args, uint32_t argCount, NPVariant *result);
+  static NPObject* Allocate(NPP npp, NPClass *aClass);
+  static NPClass _npclass;
+};
+
+
+class RVector : public RSubsettableObject
+{
+
+ public:
+  RVector(NPP instance);
+  bool Invoke(NPIdentifier name, const NPVariant *args, uint32_t argCount, NPVariant *result);
+  static NPObject* Allocate(NPP npp, NPClass *aClass);
+  static NPClass _npclass;
+};
+
+
+
+#endif //ifndef WebRObject_h_
